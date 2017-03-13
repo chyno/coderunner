31d1980a212136e5200dafd4fa8d14aaedec3f24
chyno <jchynowe1@yahoo.com>
2017-03-13 06:14:39 -0400

    unit test add users

--- codeservice/index.1.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const R = require('Ramda');
-
-exports.addTwo = function(x) {
-  return  R.add(x, 2);
-};
--- codeservice/index.js
+++ codeservice/index.js
@@ -1,6 +1,47 @@
 'use strict'
 
 const R = require('Ramda');
+const getKata = (name) =>  R.find(x => x.name === name);
+const addUserArray = R.unless(R.propIs(Array, 'users'), R.assoc('users', []));
+
+
+
+exports.addUser = R.curry((usr, kt) => {
+
+    if (!kt.users) {
+          kt.users = [];
+      }
+  let wusr = kt.users.find((x) => x.userName === usr.userName);
+  if (wusr) {
+    wusr.code = usr.code;
+    wusr.tests = usr.tests;
+  }
+  else {
+     kt.users.push(usr);  
+  }
+  return kt;
+});
+
+const getUserCodeOrDefault = R.curry((userName, doc) => {
+   let dc = doc;
+   if (doc.users) {
+     let wdoc = dc.users.find(x => x.userName === userName);
+     if (wdoc) {
+       wdoc.name = dc.name;
+       dc = wdoc;
+
+     }
+  }
+ return dc;
+});
+
+
+const log = R.curry((prefix, data) => console.log(prefix, data));
+
+//R.compose(addUser(user),addUserArray, getKata('kata 2'))(katas)
+//export { getKata, addUserArray, addUser, log, getUserCodeOrDefault };
+ 
+
 
 exports.addTwo = function(x) {
   return  R.add(x, 2);
