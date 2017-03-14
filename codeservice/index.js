da9bd6f9dcfdd5d5b3c26742bcd64cd89b1df24d
chyno <jchynowe1@yahoo.com>
2017-03-13 20:47:45 -0400

    add service class

--- codeservice/index.js
+++ codeservice/index.js
@@ -1,51 +1,48 @@
 'use strict'
+const R = require('Ramda');
 
-function KataService(db) {
-  var a =5;
+exports.KataService = function (db) {
+  this.db = db;
+  const getKata = (name) => R.find(x => x.name === name);
+  const addUserArray = R.unless(R.propIs(Array, 'users'), R.assoc('users', []));
+
+
+  const getUserCodeOrDefault = R.curry((userName, doc) => {
+    let dc = doc;
+    if (doc.users) {
+      let wdoc = dc.users.find(x => x.userName === userName);
+      if (wdoc) {
+        wdoc.name = dc.name;
+        dc = wdoc;
+
+      }
+    }
+    return dc;
+  });
+
+
+  const log = R.curry((prefix, data) => console.log(prefix, data));
   return {
-    getA: function () {
-    console.log(a);
-   }
+    addUser: R.curry((usr, kt) => {
+
+      if (!kt.users) {
+        kt.users = [];
+      }
+      let wusr = kt.users.find((x) => x.userName === usr.userName);
+      if (wusr) {
+        wusr.code = usr.code;
+        wusr.tests = usr.tests;
+      } else {
+        kt.users.push(usr);
+      }
+      return kt;
+    })
   };
 };
 
 
 
-const R = require('Ramda');
-const getKata = (name) =>  R.find(x => x.name === name);
-const addUserArray = R.unless(R.propIs(Array, 'users'), R.assoc('users', []));
 
 
-const getUserCodeOrDefault = R.curry((userName, doc) => {
-   let dc = doc;
-   if (doc.users) {
-     let wdoc = dc.users.find(x => x.userName === userName);
-     if (wdoc) {
-       wdoc.name = dc.name;
-       dc = wdoc;
-
-     }
-  }
- return dc;
-});
-
-const log = R.curry((prefix, data) => console.log(prefix, data));
-
-exports.addUser = R.curry((usr, kt) => {
-
-    if (!kt.users) {
-          kt.users = [];
-      }
-  let wusr = kt.users.find((x) => x.userName === usr.userName);
-  if (wusr) {
-    wusr.code = usr.code;
-    wusr.tests = usr.tests;
-  }
-  else {
-     kt.users.push(usr);
-  }
-  return kt;
-});
 //R.compose(addUser(user),addUserArray, getKata('kata 2'))(katas)
 //export { getKata, addUserArray, addUser, log, getUserCodeOrDefault };
-
