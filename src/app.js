e2adee50ead23e9213d0bc1e8b2f466b86066f30
chyno <jchynowe1@yahoo.com>
2017-03-15 09:46:12 -0400

    aurelia with fies

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Project Chyno';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+   { route: ['kata', 'kata'], name: 'kata',      moduleId: './kata',  nav: true, title: 'Profile' }
+      ]);
+
+    this.router = router;
+  }
+}
