8a55fafe4f9681391eb27870755c7ac6738f7c61
chyno <jchynowe1@yahoo.com>
2017-03-15 22:08:58 -0400

    add runner files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export class App {
     config.title = 'Project Chyno';
     config.map([
       { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
-   { route: ['kata', 'kata'], name: 'kata',      moduleId: './kata',  nav: true, title: 'Profile' }
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Runner' }
       ]);
 
     this.router = router;
